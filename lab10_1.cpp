96d3adc40a486e4d7f2f45798e9bbb336742fca2
Suphavej <fatkimmmm@gmail.com>
2025-12-25 01:22:05 +0700

    ya

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,27 +1,61 @@
-#include<iostream>
+#include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(){
-	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
-	return 0;
+int main() {
+    char grade;
+    int countA = 0, countB = 0, countC = 0, countD = 0, countF = 0;
+    int totalStudents = 0;
+    int studentNum = 1;
+
+    cout << "Please input grade of each student (A-F) or input 0 to exit." << endl;
+
+    while (true) {
+        cout << "Student [" << studentNum << "]: ";
+        cin >> grade;
+
+        if (grade == '0') {
+            break; // ออกจาก loop เมื่อกด 0
+        }
+
+        switch (grade) {
+            case 'A': case 'a':
+                countA++;
+                totalStudents++;
+                studentNum++;
+                break;
+            case 'B': case 'b':
+                countB++;
+                totalStudents++;
+                studentNum++;
+                break;
+            case 'C': case 'c':
+                countC++;
+                totalStudents++;
+                studentNum++;
+                break;
+            case 'D': case 'd':
+                countD++;
+                totalStudents++;
+                studentNum++;
+                break;
+            case 'F': case 'f':
+                countF++;
+                totalStudents++;
+                studentNum++;
+                break;
+            default:
+                cout << "Wrong input. Please input again." << endl;
+                // ไม่เพิ่ม studentNum เพราะเป็นการกรอกผิด
+                break;
+        }
+    }
+
+    // ส่วนการแสดงผลสรุป
+    cout << "In total " << totalStudents << " students." << endl;
+    cout << "A = " << countA << ", B = " << countB << ", C = " << countC 
+         << ", D = " << countD << ", F = " << countF << endl;
+
+    return 0;
 }
